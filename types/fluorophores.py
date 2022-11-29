f93e0e81924db0bca1e3e63f9d905bbbb044060e
Adrea Snow <adrea.snow@gmail.com>
2022-11-29 22:05:28 +1100

    fixing a bad merge

--- types/fluorophores.py
+++ types/fluorophores.py
@@ -27,7 +27,7 @@ class Fluorophores(NewEnum):
     af532     = 'AlexaFluor 532',                             'CC1C(C2=C(N1)C(=C3C(=C2)C(=C4C=C5C(=NC(C5(C)C)C)C(=C4O3)S(=O)(=O)O)C6=CC=C(C=C6)C(=O)ON7C(=O)CCC7=O)S(=O)(=O)O)(C)C', (0,0),   True
     ndi       = 'Naphthalene diimide',                        'none',                                                                                                                (0,0),   False
     nda       = 'Naphthalamide',                              'none',                                                                                                                (0,0),   False
-    pro       = 'Prodan',                                     'CCC(=O)C1=CC2=C(C=C1)C=C(C=C2)N(C)C',                                                                                 (12,12), True,  True
+    pro       = 'Prodan',                                     'CCC(=O)C1=CC2=C(C=C1)C=C(C=C2)N(C)C',                                                                                 (0,0),   True,  True
     anap      = 'l-ANAP',                                     'N[C@@H](CNC1=CC=C2C=C(C(C)=O)C=CC2=C1)C(O)=O',                                                                        (0,0),   False
     ld        = 'Laurdan',                                    'CCCCCCCCCCCC(=O)c1ccc2cc(ccc2c1)N(C)C',                                                                               (0,0),   False
     fitc5     = 'FITC',                                       'c1cc2c(cc1N=C=S)C3(c4ccc(cc4Oc5c3ccc(c5)O)O)OC2=O',                                                                   (0,0),   True
@@ -35,22 +35,22 @@ class Fluorophores(NewEnum):
     c519d     = 'Deprotonated coumarin 343/519',              'C1CC2=C3C(=C4C(=C2)C=C(C(=O)O4)C(=O)[O-])CCCN3C1',                                                                    (0,0),   True,  False, -1
     tr        = 'Texas Red',                                  'C1Cc2cc3c(c4c2N(C1)CCC4)O[C]1C2=C4N(CCC2)CCCC4=CC1=C3c1c(cc(cc1)S(=O)(=O)Cl)S([O])([O])[O]',                          (0,0),   True
     nr        = 'Nile Red',                                   'O=C1C=C2OC(C=C(N(CC)CC)C=C3)=C3N=C2C4=C1C=CC=C4',                                                                     (0,0),   True,  True
-    bod493    = 'BODIPY 493/503',                             'Cc1c2N(c(C)c1)[B-](F)(F)[N+]1c([C]2C)c(cc1C)C',                                                                       (8,8),   True,  True
+    bod493    = 'BODIPY 493/503',                             'Cc1c2N(c(C)c1)[B-](F)(F)[N+]1c([C]2C)c(cc1C)C',                                                                       (0,0),   True,  True
     mca       = 'Merocyanine 540',                            'CCCCN1C(=O)C(=C/C=C/C=C\\2/N(c3ccccc3O2)CCCS(=O)(=O)[O-])C(=O)N(C1=O)CCCC',                                           (0,0),   True,  False, -1
     dah       = 'Dansyl hydrazine',                           'CN(C)C1=CC=CC2=C1C=CC=C2S(=O)(=O)NN',                                                                                 (0,0),   True
-    daa       = 'Dansyl amide',                               'CN(C)C1=CC=CC2=C1C=CC=C2S(=O)(=O)N',                                                                                  (4,4),   True,  True
+    daa       = 'Dansyl amide',                               'CN(C)C1=CC=CC2=C1C=CC=C2S(=O)(=O)N',                                                                                  (0,0),   True,  True
     dac       = 'Dansyl chloride',                            'CN(C)C1=CC=CC2=C1C=CC=C2S(=O)(=O)Cl',                                                                                 (0,0),   True
-    az        = 'Azulene',                                    'c1cccc2cccc2c1',                                                                                                      (10,10),   True,  True,   0, States.s2 
+    az        = 'Azulene',                                    'c1cccc2cccc2c1',                                                                                                      (10,10), True,  True,   0, States.s2 
     icm       = 'Indigo carmine',                             '[O-]S(=O)(=O)c3cc4C(=O)\\C(=C2\\C(=O)c1cc(ccc1N2)S([O-])(=O)=O)Nc4cc3',                                               (0,0),   True,  True,  -2
     fl        = 'Fluorescein',                                'c1ccc2c(c1)C(=O)OC23c4ccc(cc4Oc5c3ccc(c5)O)O',                                                                        (0,0),   False
     cb        = 'Cascade blue',                               'O=S(C1=C(C2=C34)C=CC4=C(OC)C=C(S(=O)([O-])=O)C3=CC=C2C(S(=O)([O-])=O)=C1)([O-])=O',                                   (0,0),   True,  False, -3
 
     # New
     aaq       = '1-Aminoanthraquinone',                       'C1=CC=C2C(=C1)C(=O)C3=C(C2=O)C(=CC=C3)N',                                                                             (0,0),   False, True
-    ai        = '7-Azaindole',                                'C1=CC2=C(NC=C2)N=C1',                                                                                                 (8,8),   False, True
+    ai        = '7-Azaindole',                                'C1=CC2=C(NC=C2)N=C1',                                                                                                 (0,0),   False, True
 
     # possible?
-    c120      = 'Coumarin 120?',                              'CC1=CC(=O)OC2=C1C=CC(=C2)N',                                                                                          (10,10), False, True
+    c120      = 'Coumarin 120?',                              'CC1=CC(=O)OC2=C1C=CC(=C2)N',                                                                                          (0,0),   False, True
     dapi      = 'DAPI?',                                      'N=C(C1=CC2=C(C=C1)C=C(C3=CC=C(C(N)=N)C=C3)N2)N',                                                                      (0,0),   True,  True
     r800      = 'Rhodamine 800?',                             'N#CC1=C2C=C(CCC3)C4=[N+]3CCCC4=C2OC5=C1C=C6CCCN7C6=C5CCC7',                                                           (0,0),   False, True,  +1
 
