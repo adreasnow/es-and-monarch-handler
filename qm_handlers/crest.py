b7c6051095a4784ea13b9c47c7276c5eea492366
Adrea Snow <adrea.snow@gmail.com>
2022-12-18 10:53:08 +1100

    changed conda location

--- qm_handlers/crest.py
+++ qm_handlers/crest.py
@@ -13,14 +13,19 @@ def buildCRESTOpt(job:Job) -> str:
 
         if job.partner == True: slmString += '#SBATCH --qos=partner\n\n'
 
-        slmString += 'curl -s -X POST -H "Content-Type: application/json" -d \'{"value1": "CREST opt ' + job.fluorophore.name + '" , "value2": "started", "value3": ""}\' https://maker.ifttt.com/trigger/$JOBID/with/key/$JOBKEY > /dev/null\n'
-
-        slmString += '__conda_setup="$(\'/home/asnow/miniconda3/bin/conda\' \'shell.bash\' \'hook\' 2> /dev/null)"\n'
+        slmString += 'curl -s -X POST -H "Content-Type: application/json" -d \'{"value1": "CREST opt ' + job.fluorophore.name + '" , "value2": "started", "value3": "MonARCH"}\' https://maker.ifttt.com/trigger/$JOBID/with/key/$JOBKEY > /dev/null\n'
+        
+        slmString += '__conda_setup="$(\'/home/asnow/p2015120004/asnow/mambaforge/bin/conda\' \'shell.bash\' \'hook\' 2> /dev/null)"\n'
         slmString += 'if [ $? -eq 0 ]; then\n'
         slmString += '    eval "$__conda_setup"\n'
-        slmString += 'else\n    if [ -f "/home/asnow/miniconda3/etc/profile.d/conda.sh" ]; then\n'
-        slmString += '        . "/home/asnow/miniconda3/etc/profile.d/conda.sh"\n'
-        slmString += '    else\n        export PATH="/home/asnow/miniconda3/bin:$PATH"\n    fi\nfi\n'
+        slmString += 'else\n'
+        slmString += '    if [ -f "/home/asnow/p2015120004/asnow/mambaforge/etc/profile.d/conda.sh" ]; then\n'
+        slmString += '        . "/home/asnow/p2015120004/asnow/mambaforge/etc/profile.d/conda.sh"\n'
+        slmString += '    else\n'
+        slmString += '        export PATH="/home/asnow/p2015120004/asnow/mambaforge/bin:$PATH"\n'
+        slmString += '    fi\n'
+        slmString += 'fi\n\n'
+        
         slmString += 'unset __conda_setup\n\n'
         slmString += 'ulimit -s unlimited\n'
         slmString += 'export OMP_STACKSIZE=4G\n'
@@ -31,6 +36,6 @@ def buildCRESTOpt(job:Job) -> str:
 
         slmString += f'cd {job.path}\n'
         slmString += f'/usr/bin/time -v crest {job.fluorophore.name}.xyz --chrg {job.fluorophore.charge} --alpb {job.solv.xtb} {uhfString}\n\n'
-        slmString += 'curl -s -X POST -H "Content-Type: application/json" -d \'{"value1": "CREST opt ' + job.fluorophore.name + '" , "value2": "finished", "value3": ""}\' https://maker.ifttt.com/trigger/$JOBID/with/job.fluorophore.name/$JOBKEY > /dev/null\n\n'
+        slmString += 'curl -s -X POST -H "Content-Type: application/json" -d \'{"value1": "CREST opt ' + job.fluorophore.name + '" , "value2": "finished", "value3": "MonARCH"}\' https://maker.ifttt.com/trigger/$JOBID/with/job.fluorophore.name/$JOBKEY > /dev/null\n\n'
 
         return slmString
