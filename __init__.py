1b77f49ca181df0b38d5465bf7e1e0f42a0ec9c8
Adrea Snow <adrea.snow@gmail.com>
2023-02-26 15:37:24 +1100

    made the config loader a global import

--- __init__.py
+++ __init__.py
@@ -15,7 +15,7 @@ from .types.slurmJob import slurmStatus, slurmJob
 from .types.spectra import spectraType, spectrum, gaussian, deconvParams, _simpleSpectrum, spectrumSeries, trf, irf, Lifetime
 from .types.energy import Energy
 from .functions import evToNm, nmToEv, stripIllegal, smiles2xyz, script_builder
-from .functions import rmsd, fluorophores_solvents_methods, dsLoad, statusLoad
+from .functions import rmsd, fluorophores_solvents_methods, dsLoad, statusLoad, loadConfig
 from .monarchhandler import monarchHandler
 import tomli
 
